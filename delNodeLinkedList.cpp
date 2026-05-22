7b98f258f9e839ede1919fa9a07c96b5ad8b6bb6
Christopher Zarraga <christopherzarraga31@gmail.com>
2026-05-21 23:53:37 -0700

    solved LeetCode Medium 237

---
+++ delNodeLinkedList.cpp
@@ -0,0 +1,10 @@
+//LeetCode 237
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        ListNode* nextNode = node -> next;
+        node->val = node->next->val;
+        node->next = node->next->next;
+        delete nextNode;
+    }
+};
