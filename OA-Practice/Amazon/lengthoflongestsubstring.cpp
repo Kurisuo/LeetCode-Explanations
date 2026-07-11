458c4a670f7dc60a970601457d208b1c09814744
Kurisuo <christopherzarraga31@gmail.com>
2026-07-11 13:26:56 -0700

    Added Amazon OA folder. Completed LeetCode 3 Medium

---
+++ OA-Practice/Amazon/lengthoflongestsubstring.cpp
@@ -0,0 +1,42 @@
+//LeetCode: Longest Substring without repeating characters
+
+//Given a string s, find the length of the longest substring without duplicate characters.
+
+//We can use a dynamic sliding window giving us close to O(N) time
+
+#include <iostream>
+#include <unordered_map>
+#include <string>
+
+using namespace std;
+
+
+class Solution {
+    public:
+        int lengthOfLongestSubstring(std::string s) {
+        
+            unordered_map<char, int> map;
+            int longest = 0;
+            int left = 0;
+
+
+            for(int right = 0; right < s.size(); right++){
+
+                while(map.contains(s[right])){
+                    
+                    map.erase(s[left]);
+                    left++;
+                }
+
+
+                map.insert({s[right], 1});
+                longest = max(longest, right - left + 1);
+
+            }
+
+
+                return longest;
+            }
+    
+    
+    };
